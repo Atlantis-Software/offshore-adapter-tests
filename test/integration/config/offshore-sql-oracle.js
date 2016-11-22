35a8ff881b58ff05f7784e186d24afde532307e8
CorvusCorrax <renaud.cayol@gmail.com>
2016-11-22 11:32:51 +0100

    patate

--- test/integration/config/offshore-sql-oracle.js
+++ test/integration/config/offshore-sql-oracle.js
@@ -1,9 +1,12 @@
 var package = require('../../../node_modules/offshore-sql/package.json');
+var features = _.filter(package['offshoreAdapter'].features || [], function(feature) {
+  return feature !== 'autoIncrement.sequential';
+});
 module.exports = {
   name: 'offshore-sql (Oracle)',
   adapter: require('../../../node_modules/offshore-sql'),
   interfaces: package['offshoreAdapter'].interfaces,
-  features: package['offshoreAdapter'].features || [],
+  features: features,
   config: {
     host: 'localhost',
     port: 1521,
