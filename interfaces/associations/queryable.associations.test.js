3e19c4695321b8502e51933574991776568d447d
Alexandre Tiertant <alexandre.tiertant@gmail.com>
2016-12-08 14:03:13 +0100

    add multiple sort on queryable.associations.test

--- interfaces/associations/queryable.associations.test.js
+++ interfaces/associations/queryable.associations.test.js
@@ -16,12 +16,12 @@ describe('Association Interface', function() {
       Customers.push({name: 'skipLimitSort1', title: 'skipLimitSort', capital: 10000, payments: []});
       Customers.push({name: 'skipLimitSort2', title: 'skipLimitSort', capital: 25000, payments: []});
       Customers.push({name: 'skipLimitSort3', title: 'skipLimitSort', capital: 32000, payments: []});
-      Customers.push({name: 'skipLimitSort4', title: 'skipLimitSort', capital: 14000, payments: []});
+      Customers.push({name: 'skipLimitSort4', title: 'skipLimitSort', capital: 25000, payments: []});
       
-      Customers[1].payments.push({amount: 150});
-      Customers[1].payments.push({amount: 350});
-      Customers[1].payments.push({amount: 180});
-      Customers[1].payments.push({amount: 250});
+      Customers[1].payments.push({amount: 150, type: 'a'});
+      Customers[1].payments.push({amount: 350, type: 'b'});
+      Customers[1].payments.push({amount: 180, type: 'c'});
+      Customers[1].payments.push({amount: 180, type: 'd'});
 
       Associations.Customer.createEach(Customers, function(err) {
         if(err) {
@@ -36,8 +36,8 @@ describe('Association Interface', function() {
     ////////////////////////////////////////////////////
 
     it('should return the correct records', function(done) {
-      Associations.Customer.find({ where: { title: 'skipLimitSort' }, limit: 2, skip: 1, sort: 'capital asc' })
-        .populate('payments', { limit: 2, skip: 1, sort: 'amount asc'})
+      Associations.Customer.find({ where: { title: 'skipLimitSort' }, limit: 2, skip: 1, sort: {capital: 1, name: 0} })
+        .populate('payments', { limit: 2, skip: 1, sort: {amount: 1, type: 0} })
         .exec(function(err, customers) {
         assert.ifError(err);
         assert(Array.isArray(customers));
@@ -46,8 +46,8 @@ describe('Association Interface', function() {
         assert.strictEqual(customers[1].name, 'skipLimitSort2');
         assert(Array.isArray(customers[1].payments));
         assert.strictEqual(customers[1].payments.length, 2);
-        assert.strictEqual(customers[1].payments[0].amount, 180);
-        assert.strictEqual(customers[1].payments[1].amount, 250);
+        assert.strictEqual(customers[1].payments[0].type, 'd');
+        assert.strictEqual(customers[1].payments[1].type, 'c');
         done();
       });
     });
